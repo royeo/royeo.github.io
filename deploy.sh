e33618fda0c0bbaedc8889a923ad5a6c6f849ad3
royeo <ljn6176@gmail.com>
2021-01-10 21:52:53 +0800

    update article

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+set -ev
+
 hexo clean
 hexo generate
 
-set -ev
-export TZ='Asia/Shanghai'
-
-git clone -b master https://github.com/royeo/royeo.github.io.git .deploy_git
+git clone git@github.com:royeo/royeo.github.io.git .deploy_git
 mv .deploy_git/.git/ ./public/
-cd ./public
+rm -rf .deploy_git
 
+cd ./public
 git config user.name "royeo" 
 git config user.email "ljn6176@163.com" 
 git add .
 git commit -m "Travis CI Auto Builder at `date +"%Y-%m-%d %H:%M"`"
 git push
+cd ..
+rm -rf public
